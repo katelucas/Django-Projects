15d6e7e85c78a77f4a4a75cefa9803ac63f8e67e
katelucas <lucas.katie@gmail.com>
2019-06-21 06:18:05 -0400

    begin setup of authentication views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from django.urls import include
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
-
-
+]
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
